407585e53f1b568fd4bd6c76b5b2870760230b91
Алексей Шепелев <shepelev.as@phystech.edu>
2024-10-31 22:35:02 +0300

    minor fixes of README.md

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-mkdir -p tikz/resource
-xelatex -shell-escape astro-notebook.tex
-bibtex astro-notebook
-xelatex -shell-escape astro-notebook.tex
+mkdir -p tikz/resource                   # Создать папку tikz/resource, если её нет
+xelatex -shell-escape astro-notebook.tex # Собрать проект с помощью xelatex c опцией -shell-escape
+bibtex astro-notebook                    # Обработать библиографию с помощью bibtex
+xelatex -shell-escape astro-notebook.tex # Повторно собрать проект, чтобы обновилась библиография
